849fec97c6ce9ef5e47e5d97b982d280495f5aa4
Lucas Sônego <sonego_lucas@outlook.com>
2019-10-01 15:53:27 -0300

    MongoDB connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const app = express();
+const mongodbConnection = require('./private/mongodb');
 
+mongoose.connect(mongodbConnection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
 app.use(routes);
 
 app.listen(3000);
