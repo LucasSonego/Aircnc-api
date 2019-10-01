e402964a3bff1e2362548d6fae21c3ccbdd5dab5
Lucas Sônego <sonego_lucas@outlook.com>
2019-10-01 18:41:44 -0300

    feat: Booking store

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const multer = require('multer');
 
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 const uploadConfig = require('./config/upload');
 
 
@@ -12,5 +14,9 @@ const upload = multer(uploadConfig);
 routes.post('/sessions', SessionController.store);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 routes.get('/spots', SpotController.index);
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store)
 
 module.exports = routes;
