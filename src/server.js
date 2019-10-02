387c5632794a76f5a27da35c04a1551bef0c538b
Lucas Sônego <sonego_lucas@outlook.com>
2019-10-02 15:31:28 -0300

    Added CORS pakage

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const mongodbConnection = require('./private/mongodb');
@@ -10,6 +11,7 @@ mongoose.connect(mongodbConnection, {
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
